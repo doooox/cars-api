2ab79a0b3cdcc14cafec760db5c447da8abcc87a
doooox <dtopic12@gmail.com>
2022-11-16 00:07:05 +0100

    Add CarsController, add group route, implemented index, show, store (with reques), update (with reques), delete

---
+++ app/Http/Controllers/CarsController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateCarRequest;
+use App\Http\Requests\UpdateCarRequest;
+use Illuminate\Http\Request;
+use App\Models\Car;
+
+class CarsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cars = Car::all();
+        return $cars;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateCarRequest $request, Car $car)
+    {
+        $data = $request->validated();
+        $car = Car::create($data);
+        return $car;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Car $car)
+    {
+        return $car;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCarRequest $request, Car $car)
+    {
+        $data = $request->validate();
+        $car->update($data);
+        return $car;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Car $car)
+    {
+        $car->delete();
+        return "Car deleted";
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarsController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(CarsController::class)->group(function () {
+    Route::resource('cars', CarsController::class);
+    Route::delete('cars/{car}', [CarsController::class, 'destroy']);
+    Route::put('cars/{car}', [CarsController::class, 'update']);
 });
